748b42c0786fbd7354330ef46d422db87cc06701
MinexAutomation <minex.automation@gmail.com>
2019-06-03 17:19:19 -0400

    Added IEnumerable<T>.ForEach() extension method.

---
+++ source/R5T.NetStandard.Extensions/Code/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.NetStandard
+{
+    public static class IEnumerableExtensions
+    {
+    }
+}
+
+
+namespace R5T.NetStandard.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Performs the specified action on each element of an enumerable.
+        /// </summary>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+            {
+                action(element);
+            }
+        }
+    }
+}
