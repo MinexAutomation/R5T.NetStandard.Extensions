7fa16ded5531c127ee14ff5e08acf6ca68bfa5be
MinexAutomation <minex.automation@gmail.com>
2019-06-13 17:29:54 -0400

    Added ILogger, IServiceCollection, and IServiceProvider extensions.

---
+++ source/R5T.NetStandard.Extensions/Code/Extensions/ILoggerExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.NetStandard
+{
+    public static class ILoggerExtensions
+    {
+        public static void LogAddingTransientService<TService>(this ILogger log)
+        {
+            log.LogAddingTransientService(typeof(TService));
+        }
+
+        public static void LogAddingTransientService(this ILogger log, Type type)
+        {
+            log.LogInformation($@"{type} - TRANSIENT. Adding service...");
+        }
+
+        public static void LogAddingScopedService<TService>(this ILogger log)
+        {
+            log.LogAddingScopedService(typeof(TService));
+        }
+
+        public static void LogAddingScopedService(this ILogger log, Type type)
+        {
+            log.LogInformation($@"{type} - SCOPED. Adding service...");
+        }
+
+        public static void LogAddingSingletonService<TService>(this ILogger log)
+        {
+            log.LogAddingSingletonService(typeof(TService));
+        }
+
+        public static void LogAddingSingletonService(this ILogger log, Type type)
+        {
+            log.LogInformation($@"{type} - SINGLETON. Adding service...");
+        }
+
+        public static void LogAddingService(this ILogger log, string serviceName)
+        {
+            log.LogInformation($@"{serviceName.ToUpperInvariant()} - Adding service...");
+        }
+
+        public static void LogUse(this ILogger log, string serviceName)
+        {
+            log.LogInformation($@"{serviceName} - Using service.");
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Extensions/Code/Extensions/IServiceProviderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+
+namespace R5T.NetStandard
+{
+    public static class IServiceProviderExtensions
+    {
+        public static IConfiguration GetConfiguration(this IServiceProvider serviceProvider)
+        {
+            var output = serviceProvider.GetRequiredService<IConfiguration>();
+            return output;
+        }
+
+        /// <summary>
+        /// Directly gets the <typeparamref name="TOptions"/> value from the <see cref="IOptions{TOptions}"/> service.
+        /// </summary>
+        public static TOptions GetOptions<TOptions>(this IServiceProvider serviceProvider)
+            where TOptions : class, new()
+        {
+            var optionsOptions = serviceProvider.GetRequiredService<IOptions<TOptions>>();
+            var options = optionsOptions.Value;
+            return options;
+        }
+    }
+}
