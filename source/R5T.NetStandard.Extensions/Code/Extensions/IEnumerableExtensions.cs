b7e132a91d4d4b89a1035493209ad4717593d5f9
MinexAutomation <minex.automation@gmail.com>
2019-06-14 14:39:43 -0400

    Added IEnumerable.And() extension method to allow enumeration over an enumeration made from both an enumeration and extra elements.

--- source/R5T.NetStandard.Extensions/Code/Extensions/IEnumerableExtensions.cs
+++ source/R5T.NetStandard.Extensions/Code/Extensions/IEnumerableExtensions.cs
@@ -15,6 +15,22 @@ namespace R5T.NetStandard.Extensions
     public static class IEnumerableExtensions
     {
         /// <summary>
+        /// Allows adding a few extra items to an enumerable.
+        /// </summary>
+        public static IEnumerable<T> And<T>(this IEnumerable<T> enumerable, params T[] extras)
+        {
+            foreach (var item in enumerable)
+            {
+                yield return item;
+            }
+
+            foreach (var item in extras)
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
         /// Performs the specified action on each element of an enumerable.
         /// </summary>
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
